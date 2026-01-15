a4018b1628629733886438841455ab91ccd64a7c
HighOnKohi <mgutierrezvinccent69@gmail.com>
2026-01-15 19:56:19 +0800

    tite

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Login from "./pages/Login.jsx";
+import Login from "./pages/Login/Login.jsx";
 import Homepage from "./pages/Hompage/Homepage.jsx";
+import Sidebar from "./GlobalComponents/Sidebar";
+import Header from "./GlobalComponents/Header";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Homepage />} />
+      <Route
+        path="/home"
+        element={
+          <div className="layout">
+            <Sidebar />
+
+            <main className="main">
+              <Header />
+              <Homepage />
+            </main>
+          </div>
+        }
+      />
 
       {/* <Route path="/unauthorized" element={<Unauthorized />} /> */}
 
