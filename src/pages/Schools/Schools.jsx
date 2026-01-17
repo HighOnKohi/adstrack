058407e03b80037ced60402cfd3871b3a310f959
Caleberr <nashordrake1@gmail.com>
2026-01-17 12:08:19 +0800

    ew

--- src/pages/Schools/Schools.jsx
+++ src/pages/Schools/Schools.jsx
@@ -7,6 +7,8 @@ function Schools() {
                 <h1> School Directory </h1>
                 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. </p>
             </div>
+
+            <button className="register-school-button"> Register School </button>
         </div>
     );
 }
